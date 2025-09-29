e3d1727482680b0ea9f48f966a5d9268cb455d12
Jose Luis <jrodesc1511@gmail.com>
2025-09-29 18:01:43 +0200

    ejercicio 8

--- UD2/Ejercicios/EJ08/funciones.js
+++ UD2/Ejercicios/EJ08/funciones.js
@@ -1,5 +1,24 @@
 function resolucion() {
     let numero = parseInt(document.getElementById("numero").value);
+    let mensaje ="El numero es: ";
 
-    
+
+
+    if(numero > 0) {
+        mensaje += "positivo, ";
+    } else if(numero < 0) {
+        mensaje += "negativo, ";
+    } else {
+        mensaje += "es cero, ";
+    }
+
+    if(numero%2 === 0) {
+        mensaje += "par";
+    } else {
+        mensaje += "impar";
+    }
+
+
+    document.getElementById("respuesta").innerHTML = mensaje;
+
 }
