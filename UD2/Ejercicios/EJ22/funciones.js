7c5046a59adc414e4693c2c94c3f28f026bb1b9b
Jose Luis <jrodesc1511@gmail.com>
2025-10-01 14:27:23 +0200

    ejercicio 22 iniciado

---
+++ UD2/Ejercicios/EJ22/funciones.js
@@ -0,0 +1,15 @@
+function mayoresEdad() {
+    let nombres = [];
+    let edades = [];
+
+    nombres.push(document.getElementById("n1").value);
+    nombres.push(document.getElementById("n2").value);
+    nombres.push(document.getElementById("n3").value);
+    nombres.push(document.getElementById("n4").value);
+    nombres.push(document.getElementById("n5").value);
+    nombres.push(document.getElementById("n6").value);
+    nombres.push(document.getElementById("n7").value);
+    nombres.push(document.getElementById("n8").value);
+    nombres.push(document.getElementById("n9").value);
+    nombres.push(document.getElementById("n10").value);
+}
