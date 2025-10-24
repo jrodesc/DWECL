5e64438f26850a3fa6680d2addb5014e543cf536
Jose Luis <jrodesc1511@gmail.com>
2025-10-24 10:59:10 +0200

    tarea 4.3

---
+++ UD4/tarea4.3/Ejercicio1/funciones.js
@@ -0,0 +1,41 @@
+function Animal(nombre) {
+  this.nombre = nombre;
+}
+
+Animal.prototype.hacerRuido = function () {
+  return "Soy " + this.nombre + " y soy un animal y hago...";
+};
+
+function Zebra(nombre, rayas) {
+  Animal.call(this, nombre);
+
+  this.rayas = rayas;
+}
+Zebra.prototype = Object.create(Animal.prototype);
+Zebra.prototype.constructor = Zebra;
+
+Zebra.prototype.hacerRuido = function () {
+    console.log(
+      "Soy " + this.nombre + " y soy un animal, tengo la propiedad "+this.rayas +"y hago rudios al caiman"
+    )
+}
+
+function Caiman(nombre, morder) {
+  Animal.call(this, nombre);
+  this.rayas = morder;
+}
+Caiman.prototype = Object.create(Animal.prototype);
+Caiman.prototype.constructor = Caiman;
+
+Caiman.prototype.hacerRuido = function() {
+    console.log(
+      "Soy " + this.nombre + " y soy un animal "+this.morder +"y hago mordeduras a la zebra"
+    )
+}
+
+
+const miZebra = new Zebra("Julio", 100);
+
+const miCaiman = new Caiman("Angel", true);
+miCaiman.hacerRuido();
+miZebra.hacerRuido();
