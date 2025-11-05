7d09520b02cbf55f7b95425037339239db35fa0c
Jose Luis <jrodesc1511@gmail.com>
2025-11-05 14:25:10 +0100

    ejercicio de singleton

--- UD4/tarea4.4/patrones-solid-ts-con-patrones/src/patterns/Singleton.ts
+++ UD4/tarea4.4/patrones-solid-ts-con-patrones/src/patterns/Singleton.ts
@@ -1,6 +1,9 @@
 class Configuracion {
   private static instancia: Configuracion;
-  private constructor(public readonly apiUrl: string) {}
+
+  private static cosa: String;
+  private constructor(public apiUrl: string) {
+  }
 
   static getInstancia(): Configuracion {
     if (!Configuracion.instancia) {
@@ -8,9 +11,26 @@ class Configuracion {
     }
     return Configuracion.instancia;
   }
+
+  static getCosa(): String {
+    return this.cosa;
+  }
+
+  static setCosa(cosaNueva: String): void {
+    if(!Configuracion.cosa) {
+      Configuracion.cosa = cosaNueva;
+    }
+  }
 }
 
+
+Configuracion.setCosa("Cosa Nueva");
+const cosa1 = Configuracion.getCosa();
+const cosa2 = Configuracion.getCosa();
 const conf1 = Configuracion.getInstancia();
 const conf2 = Configuracion.getInstancia();
 
+console.log(cosa2);
+console.log(cosa1);
+console.log(cosa1 === cosa2)
 console.log(conf1 === conf2); // true ✅
